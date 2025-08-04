a06698e353f7f586673a7fe71d5df73857f357be
vinanamira <mzssdaru@gmail.com>
2025-08-04 18:08:02 +0700

    Update: Change url logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
                 <h1>
-                    <a href="/" className="logo">
+                    <a href="https://vinanamira.github.io/portfolio/" className="logo">
                         <img
                             src={Logo}
                             width={40}
